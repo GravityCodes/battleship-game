d573926150cc5970d4e0870c19582ebe7ea1e371
Johan <johanotr@gmail.com>
2024-10-29 18:03:21 -0400

    feat: create placeShip functions for gameBoard class

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,3 +1,5 @@
+import Ship from "./ship"
+
 export default class GameBoard {
     constructor(){
         this.board = [
@@ -12,24 +14,45 @@ export default class GameBoard {
             [0,0,0,0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0,0,0,0],
         ];
+        this.ships = {};
     }
 
     //create ship instance and place it on board.
     
     placeShipVertical(size, coords){
-
         
+        const ship = new Ship(size);
+        
+        //Place ship in ships object with the name being the coords.
+        this.ships[coords] = ship;
+
+        //Place ship object in board vertically.
+        for(let i = coords[0]; i < size; i++){
+            this.board[i][coords[1]] = coords;
+        }
 
     }
 
     placeShipHorizontal(size, coords){
+  
+        const ship = new Ship(size);
+        
+        //Place ship in ships object with the name being the coords.
+        this.ships[coords] = ship;
 
+        //Place ship object in board vertically.
+        for(let i = coords[1]; i < size; i++){
+            this.board[coords[0]][i] = coords;
+        }
 
     }
 
-    hasShip(){
-        this.board.forEach(array => {
-            array.includes()
-        })
+    
+
+
+    hasShipAt(coords){
+  
+        return this.board[coords[0]][coords[1]] != 0 ? true : false;
     }
+
 }
--- test/gameboard.test.js
+++ test/gameboard.test.js
@@ -4,9 +4,29 @@ const gameBoard = new GameBoard();
 
 describe("GameBoard class functions work", () => {
 
-    test("placeShip() places a ship instance to the board", () => {
+    test("placeShipVertical() places a ship instance to the board", () => {
         gameBoard.placeShipVertical(3,[0,0]);
 
-        expect(gameBoard.hasShips()).toBe(true);
+        expect(gameBoard.hasShipAt([0,0])).toBe(true);
     });
+
+    test("placeShipHorizontal() places a ship instance to the board", () => {
+        gameBoard.placeShipHorizontal(3,[0,2]);
+
+        expect(gameBoard.hasShipAt([0,2])).toBe(true);
+    });
+
+    test("receiveAttack() hits ships correctly", () => {
+                
+        expect(gameBoard.receiveAttack([0,0])).toBe(true);
+
+    });
+
+    test("receiveAttack() misses ships correctly", () => {
+                
+        expect(gameBoard.receiveAttack([5,0])).toBe(false);
+
+    });
+
+
 });
